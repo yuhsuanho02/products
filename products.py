44bb75bf96cd74626b0dddd230b57236b3887d6f
Adam Ho <yuhsuanho02@gmail.com>
2022-01-15 21:02:33 +0800

    establish csv

--- products.py
+++ products.py
@@ -3,9 +3,15 @@ while True:
     name = input('請輸入商品名稱: ')
     if name == 'q':
         break
-    price = input('請輸入商品價錢: ')
-    p = []
-    p.append(name)
-    p.append(price)
-    products.append(p)
+    price = input('請輸入商品價格: ')
+    products.append([name, price])
 print(products)
+
+for p in products:
+    print(p[0], '的價格是', p[1])
+
+with open('products.csv', 'w') as f:
+    for p in products:
+        f.write(p[0] + ',' + p[1] + '\n')
+
+
