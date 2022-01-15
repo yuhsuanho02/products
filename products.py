ae95527ec29894cd05ae8f9296dc1bbc8f004457
Adam Ho <yuhsuanho02@gmail.com>
2022-01-15 22:18:36 +0800

    add check file exist

--- products.py
+++ products.py
@@ -1,12 +1,18 @@
-#讀取檔案
+import os #operating system
+
 products = []
-with open('products.csv', 'r', encoding='utf-8') as f:
-    for line in f:
-        if '商品,價格' in line:
-            continue #繼續下一回,略過
-        name, price = line.strip().split(',')
-        products.append([name,price])
-print(products)
+if os.path.isfile('products.csv'): #檢查檔案在不在
+    print('找到了')
+    with open('products.csv', 'r', encoding='utf-8') as f: #讀取檔案
+        for line in f:
+            if '商品,價格' in line:
+                continue #繼續下一回,略過
+            name, price = line.strip().split(',')
+            products.append([name,price])
+    print(products)
+
+else:
+    print('找不到檔案....')
 
 #讓使用者輸入
 while True:
